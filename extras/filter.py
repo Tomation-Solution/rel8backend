0f30d7c1d5966bb707605784fbcf7fa201136a99
markothedev <markothedevmail@gmail.com>
2023-02-18 08:57:03 +0100

    added a new filter param to gallery

--- extras/filter.py
+++ extras/filter.py
@@ -5,8 +5,9 @@ import django_filters
 
 
 class GalleryV2Filter(django_filters.FilterSet):
+    not_chapters  = django_filters.BooleanFilter(field_name='chapters',lookup_expr='isnull')
 
     # date_taken= django_filters.DateFilter(field_name='date_taken')
     class Meta:
         model = models.GalleryV2
-        fields = ['date_taken','chapters']
+        fields = ['date_taken','chapters','not_chapters']
