adb90066257d7ff5817a75ac9ad8b4eadc1a8dc0
markothedev <markothedevmail@gmail.com>
2023-02-20 22:50:09 +0100

    fix url spelling

--- servicerequest/urls.py
+++ servicerequest/urls.py
@@ -11,7 +11,7 @@ route.register('Change_of_name',views.ChangeOfNameViewset)
 route.register('merger_of_companies',views.MergerOfCompaniesViewset)
 route.register('deactivation_of_membership',views.DeactivationOfMembershipViewset)
 route.register('product_manufacturing_update',views.ProductManufacturingUpdateViewset)
-route.register('factoryLocation_update',views.FactoryLocationUpdateViewset)
+route.register('factory_location_update',views.FactoryLocationUpdateViewset)
 
 urlpatterns  =[ 
 ] + route.urls
