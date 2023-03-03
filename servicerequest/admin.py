5118d32b64046afee6a3d4bc09482b9654ea9e00
markothedev <markothedevmail@gmail.com>
2023-03-03 04:04:23 +0100

    added available servicereques models to admin

--- servicerequest/admin.py
+++ servicerequest/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+admin.site.register(models.ChangeOfName)
+admin.site.register(models.MemberOfReissuanceOfCertificate)
+admin.site.register(models.MergerOfCompanies)
+admin.site.register(models.DeactivationOfMembership)
+admin.site.register(models.ProductManufacturingUpdate)
+admin.site.register(models.FactoryLocationUpdate)
