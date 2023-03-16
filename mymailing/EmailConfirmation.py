98dcbebac9e0d928be7739497907e5f3992566a1
markothedev <markothedevmail@gmail.com>
2023-03-16 15:16:00 +0100

    now proxies will be sent mail if they are invited

--- mymailing/EmailConfirmation.py
+++ mymailing/EmailConfirmation.py
@@ -7,6 +7,7 @@ from .tokens import account_activation_token
 from django.core.mail import send_mail
 import os,json
 from event import models as event_models
+from meeting import models as meeting_models
 from account.models import user as models_user
 from django.db import connection
 
@@ -24,15 +25,12 @@ def activateEmail(user,to_email):
     send_mail(mail_subject,'',os.environ['domain_mail'],recipient_list=[to_email],html_message=message,)
 
 def sendInvitationMail(user,event:event_models.Event,event_proxy_attendies:event_models.EventProxyAttendies):
-    mail_subject = f'Invitation for {event.name}'
+    mail_subject = f'Invitation for {event.name} Event'
     person_that_invite_you_email= event_proxy_attendies.event_due_user.user.email
     person_that_invite_you_full_name = models_user.Memeber.objects.get(user=event_proxy_attendies.event_due_user.user.id)
     def func(item):return item.get('email')
     to_emails =map(func,event_proxy_attendies.participants)
-    print({
-        'value':event_proxy_attendies.participants,
-        'type':type(event_proxy_attendies.participants)
-    })
+
     data ={
         'person_that_invite_you_email':person_that_invite_you_email,
         'person_that_invite_you_full_name':person_that_invite_you_full_name.full_name,
@@ -43,3 +41,21 @@ def sendInvitationMail(user,event:event_models.Event,event_proxy_attendies:event
     message = render_to_string('proxy_event_mail.html',context=data)
 
     send_mail(mail_subject,'',os.environ['domain_mail'],recipient_list=to_emails,html_message=message,)
+
+
+
+def sendMeetingInvitationMail(user,meeting:meeting_models.Meeting,meeting_proxy_attendies:meeting_models.MeetingProxyAttendies):
+    mail_subject = f'Invitation for {meeting.name} Meeting'
+    person_that_invite_you_email= meeting_proxy_attendies.member.user.email
+    person_that_invite_you_full_name = meeting_proxy_attendies.member.full_name
+
+
+    data ={
+        'person_that_invite_you_email':person_that_invite_you_email,
+        'person_that_invite_you_full_name':person_that_invite_you_full_name.full_name,
+        'meeting_link_or_address':meeting.addresse,
+        'name':meeting.addresse,
+        'short_name':connection.schema_name
+        }
+    
+    message = render_to_string('proxy_meeting_mail.html',context=data)
