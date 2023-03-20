5751f561419de9b1bff0727b380c397625024573
markothedev <markothedevmail@gmail.com>
2023-03-20 02:21:29 +0100

    added a function that adds memebers to chapter automatically

--- account/views/auth.py
+++ account/views/auth.py
@@ -218,15 +218,27 @@ class ManageMemberValidation(viewsets.ViewSet):
         
         if user_models.UserMemberInfo.objects.filter(value=MEMBERSHIP_NO).exists():
             raise CustomError({'error':'Membership info has been registered already'})
-        
-        
+        chapter=None
+        for key in request.data.keys():
+            if key == 'chapter':
+                chapter = request.data[key]  
+        if chapter is None:
+            raise CustomError({'error':'Chapter not found please reach out to admin'})
         if(len(alum_db['usersInfo'][0].keys())==len(request.data.keys())):
             if get_user_model().objects.filter(email=email).exists():raise CustomError({'error':'email already exists'})
+            if not auth_models.Chapters.objects.filter(name__icontains=chapter):
+                raise CustomError({'error':'chapter not found'})
+            chapter_instance = auth_models.Chapters.objects.filter(name__icontains=chapter).first()
             user = get_user_model().objects.create_user(
                 email=email,
                 user_type='members',
-                password =password
+                password =password,
+                
             )
+            user.chapter  = chapter_instance
+            user.save()
+            "we going to add chapters if there is"
+
             member = user_models.Memeber.objects.create(
                 user =user,
                 alumni_year=alumni_year
