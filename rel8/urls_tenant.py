a6a9d439a3da69ce6718bcbc073e4dd34f8a09b6
XKolz <72173277+XKolz@users.noreply.github.com>
2024-07-29 19:03:49 +0100

    Url fixes

--- rel8/urls_tenant.py
+++ rel8/urls_tenant.py
@@ -40,8 +40,8 @@ urlpatterns = [
     path('interswitch_payment/',include('interswitchapp.urls')),
     path('mailing/',include('mymailing.urls')),
 
-    path('tenant/prospectivemember/',include('prospectivemember.urls')),
-    path('inbox/notifications/', include('notifications.urls', namespace='notifications')),
+    path('tenant/prospectivemember/',include('prospectivemember.urls'))
+    # path('inbox/notifications/', include('notifications.urls', namespace='notifications')),
 
 ]
 
