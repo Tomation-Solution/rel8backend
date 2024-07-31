28fffe9f89c09af9f689d9e547cabd68003d0b02
Arafat Olayiwola <harof.dev@gmail.com>
2024-07-31 14:39:50 +0100

    fixing chat consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -18,108 +18,13 @@ from account.models import user as user_related_models
 
 logger = logging.getLogger(__name__)
 
-# class ChatRoomConsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-#         self.room_name = self.scope['url_route']['kwargs']['room_name']
-#         self.room_group_name = self.room_name
-#         self.tenant = self.scope['url_route']['kwargs']['tenant_name']
-#         self.user_full_name = None
-#         await self.channel_layer.group_add(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#         await self.accept()
-
-#     async def disconnect(self, close_code):
-#         await self.channel_layer.group_discard(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-
-#     async def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json['message']
-#         send_user_id = text_data_json['send_user_id']
-#         # tenant_name
-#         # print({'schema anme':connection.schema_name})
-#         logger.info({'schema anme':self.tenant })
-#         "here we setting the schema using the short name tha was sent from the front end"
-        
-#         # firstuserid,seconduserid = self.room_name.splite('and') 
-#         # await self.send_pusher(send_user_id)
-#         await self.validate_user(send_user_id)
-#         await self.create_chat(send_user_id,message)
-
-#         await self.channel_layer.group_send(
-#             self.room_group_name,
-#             {
-#                 'type': 'chatroom_message',
-#                 'message': message,
-#                 'send_user_id': send_user_id,
-#             }
-#         )
-
-#     @sync_to_async
-#     def validate_user(self,send_user_id):
-#         connection.set_schema(schema_name=self.tenant)
-#         "check if user id exist in the data base"
-#         logger.info({'send_user_id':send_user_id})
-#         if not get_user_model().objects.filter(id=send_user_id).exists():
-#             logger.error('does not exist')
-#             raise CustomError(
-#             message='User does not exist',
-#             status_code=status.HTTP_400_BAD_REQUEST
-#         )
-        
-    
-#     @sync_to_async
-#     def fill_user_name(self,send_user_id:int):
-#         user = get_user_model().objects.get(id=send_user_id)
-#         user_full_name = None
-#         if user.user_type== 'members':
-#             user_full_name = user.memeber.full_name
-#         else:
-#             user_full_name= f'admin: {user.email}'
-#         return user_full_name
-
-#     @sync_to_async
-#     def create_chat(self,send_user_id,message):
-#         "we get or create a group name"
-#         connection.set_schema(schema_name=self.tenant)
-#         chat_room ,created=  models.ChatRoom.objects.get_or_create(
-#             room_name=self.room_group_name
-#         )
-#         chat_room.save()
-#         "save the message to that group and u can add it wil the user id"
-#         user = get_user_model().objects.get(id=send_user_id)
-#         chat = models.Chat.objects.create(
-#             chat_room=chat_room,
-#             message=message,
-#             user=user
-#         )
-#         chat.save()
-
-#     async def chatroom_message(self, event):
-
-#         message = event['message']
-#         send_user_id = event['send_user_id']
-#         full_name = await self.fill_user_name(send_user_id) 
-#         await self.send(text_data=json.dumps({
-#             'message': message,
-#             'send_user_id': send_user_id,
-#             'full_name':full_name
-
-#         }))
-
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = self.room_name
         self.tenant = self.scope['url_route']['kwargs']['tenant_name']
         self.user_full_name = None
-        
         await self.validate_room()
-        
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -134,16 +39,17 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json.get('message')
-        send_user_id = text_data_json.get('send_user_id')
-
-        if not message or not send_user_id:
-            return
+        message = text_data_json['message']
+        send_user_id = text_data_json['send_user_id']
+        # tenant_name
+        # print({'schema anme':connection.schema_name})
+        logger.info({'schema anme':self.tenant })
+        "here we setting the schema using the short name tha was sent from the front end"
         
-        logger.info({'schema name': self.tenant})
-
+        # firstuserid,seconduserid = self.room_name.splite('and') 
+        # await self.send_pusher(send_user_id)
         await self.validate_user(send_user_id)
-        await self.create_chat(send_user_id, message)
+        await self.create_chat(send_user_id,message)
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -153,17 +59,6 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
                 'send_user_id': send_user_id,
             }
         )
-
-    @sync_to_async
-    def validate_user(self, send_user_id):
-        connection.set_schema(schema_name=self.tenant)
-        if not get_user_model().objects.filter(id=send_user_id).exists():
-            logger.error('User does not exist')
-            raise CustomError(
-                message='User does not exist',
-                status_code=status.HTTP_400_BAD_REQUEST
-            )
-        
     @sync_to_async
     def validate_room(self):
         """
@@ -196,35 +91,57 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             )
 
 
-    @sync_to_async
-    def fill_user_name(self, send_user_id: int):
-        user = get_user_model().objects.get(id=send_user_id)
-        if user.user_type == 'members':
-            return user.member.full_name
-        return f'admin: {user.first_name}'
 
     @sync_to_async
-    def create_chat(self, send_user_id, message):
+    def validate_user(self,send_user_id):
         connection.set_schema(schema_name=self.tenant)
-        chat_room, created = models.ChatRoom.objects.get_or_create(
+        "check if user id exist in the data base"
+        logger.info({'send_user_id':send_user_id})
+        if not get_user_model().objects.filter(id=send_user_id).exists():
+            logger.error('does not exist')
+            raise CustomError(
+            message='User does not exist',
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+        
+    
+    @sync_to_async
+    def fill_user_name(self,send_user_id:int):
+        user = get_user_model().objects.get(id=send_user_id)
+        user_full_name = None
+        if user.user_type== 'members':
+            user_full_name = user.memeber.full_name
+        else:
+            user_full_name= f'admin: {user.email}'
+        return user_full_name
+
+    @sync_to_async
+    def create_chat(self,send_user_id,message):
+        "we get or create a group name"
+        connection.set_schema(schema_name=self.tenant)
+        chat_room ,created=  models.ChatRoom.objects.get_or_create(
             room_name=self.room_group_name
         )
+        chat_room.save()
+        "save the message to that group and u can add it wil the user id"
         user = get_user_model().objects.get(id=send_user_id)
-        models.Chat.objects.create(
+        chat = models.Chat.objects.create(
             chat_room=chat_room,
             message=message,
             user=user
         )
+        chat.save()
 
     async def chatroom_message(self, event):
+
         message = event['message']
         send_user_id = event['send_user_id']
-        full_name = await self.fill_user_name(send_user_id)
-        
+        full_name = await self.fill_user_name(send_user_id) 
         await self.send(text_data=json.dumps({
             'message': message,
             'send_user_id': send_user_id,
-            'full_name': full_name
+            'full_name':full_name
+
         }))
 
 
