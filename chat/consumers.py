1567ec3ae215fe1b13bf07a2822c9e07fb23aa98
Arafat Olayiwola <harof.dev@gmail.com>
2024-07-31 14:55:14 +0100

    ensuring third user do not join private chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -20,10 +20,9 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = self.room_name
         self.tenant = self.scope['url_route']['kwargs']['tenant_name']
-        self.user_full_name =None
-
-        self.validate_room()
+        self.user = self.scope['user']  # The user from the scope
         
+        await self.validate_user(self.user.id)
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -35,54 +34,14 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-    # async def send_pusher(self,senderID,message='hellow'):
-        
-    #     'note this is a one on one chat so we get the ids all i need to do is to send the notifciation to the reciver'
-        # firstuserid,seconduserid = self.room_name.splite('and') 
-        # firstuserid = int(firstuserid)
-        # seconduserid = int(seconduserid)
 
-        # reciver_id = -1
-
-        # if seconduserid != int(senderID):
-        #     'this person is a reciver'
-        #     reciver_id = seconduserid
-        # if firstuserid != int(senderID):
-        #     'this person is a reciver'
-        #     reciver_id = firstuserid
-        # alert ={
-        #         'title':'membership app',
-        #         'body': message,
-        #     }
-        # response= beams_client.publish_to_users(
-        # user_ids=[f'{reciver_id}'],
-        # publish_body={
-        # 'apns': {
-        #     'aps': {
-        #     'alert': alert,
-        #     },
-        # },
-        # 'fcm': {
-        #     'notification': alert,
-        # },
-        # 'web': {
-        #     'notification':alert,
-        # },
-        # },
-        # )
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         send_user_id = text_data_json['send_user_id']
-        # tenant_name
-        # print({'schema anme':connection.schema_name})
-        print({'schema anme':self.tenant })
-        "here we setting the schema using the short name tha was sent from the front end"
         
-        # firstuserid,seconduserid = self.room_name.splite('and') 
-        # await self.send_pusher(send_user_id)
         await self.validate_user(send_user_id)
-        await self.create_chat(send_user_id,message)
+        await self.create_chat(send_user_id, message)
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -94,88 +53,210 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         )
 
     @sync_to_async
-    def validate_user(self,send_user_id):
+    def validate_user(self, send_user_id):
         connection.set_schema(schema_name=self.tenant)
-        "check if user id exist in the data base"
-        print({'send_user_id':send_user_id})
-        if not get_user_model().objects.filter(id=send_user_id).exists():
-            print('does not exist')
+        users_in_room = self.room_name.split('and')
+        if str(send_user_id) not in users_in_room:
             raise CustomError(
-            message='User does not exist',
-            status_code=status.HTTP_400_BAD_REQUEST
-        )
-        
-    
+                message='Access Denied: You are not allowed to join this room',
+                status_code=status.HTTP_403_FORBIDDEN
+            )
+
     @sync_to_async
-    def fill_user_name(self,send_user_id:int):
-        user =get_user_model().objects.get(id=send_user_id)
-        user_full_name = None
-        if user.user_type== 'members':
-            user_full_name = user.memeber.full_name
-        else:
-            user_full_name= f'admin: {user.first_name}'
-        return user_full_name
-    @sync_to_async
-    def create_chat(self,send_user_id,message):
-        "we get or create a group name"
+    def create_chat(self, send_user_id, message):
         connection.set_schema(schema_name=self.tenant)
-        chat_room ,created=  models.ChatRoom.objects.get_or_create(
+        chat_room, created = models.ChatRoom.objects.get_or_create(
             room_name=self.room_group_name
         )
-        chat_room.save()
-        "save the message to that group and u can add it wil the user id"
         user = get_user_model().objects.get(id=send_user_id)
-        chat = models.Chat.objects.create(
+        models.Chat.objects.create(
             chat_room=chat_room,
             message=message,
             user=user
         )
-        chat.save()
 
     async def chatroom_message(self, event):
-
         message = event['message']
         send_user_id = event['send_user_id']
-        full_name = await self.fill_user_name(send_user_id) 
+        full_name = await self.fill_user_name(send_user_id)
         await self.send(text_data=json.dumps({
             'message': message,
             'send_user_id': send_user_id,
-            'full_name':full_name
-
+            'full_name': full_name
         }))
 
     @sync_to_async
-    def validate_room(self):
-        """
-        Validate that the user has access to the specified chat room.
-        """
-        room_name = self.room_name
-        user = self.scope['user']
+    def fill_user_name(self, send_user_id: int):
+        user = get_user_model().objects.get(id=send_user_id)
+        if user.user_type == 'members':
+            user_full_name = user.member.full_name
+        else:
+            user_full_name = f'admin: {user.first_name}'
+        return user_full_name
 
-        # Set the tenant schema
-        connection.set_schema(schema_name=self.tenant)
 
-        if room_name is None:
-            logger.error('Chat Room name is required')
-            raise CustomError(
-                message='Chat Room name is required',
-                status_code=status.HTTP_400_BAD_REQUEST
-            )
 
-        # Check if the chat room exists and if the user is part of it
-        chat_room_exists = models.Chat.objects.filter(
-            chat_room__room_name=room_name, 
-            user=user
-        ).exists()
+# class ChatRoomConsumer(AsyncWebsocketConsumer):
+    # async def connect(self):
+    #     self.room_name = self.scope['url_route']['kwargs']['room_name']
+    #     self.room_group_name = self.room_name
+    #     self.tenant = self.scope['url_route']['kwargs']['tenant_name']
+    #     self.user_full_name =None
 
-        if not chat_room_exists:
-            logger.error('Access Denied: User does not have access to the chat room')
-            raise CustomError(
-                message='Access Denied: You do not have access to the chat room',
-                status_code=status.HTTP_403_FORBIDDEN
-            )
+    #     self.validate_room()
+        
+    #     await self.channel_layer.group_add(
+    #         self.room_group_name,
+    #         self.channel_name
+    #     )
+    #     await self.accept()
 
-    pass
+    # async def disconnect(self, close_code):
+    #     await self.channel_layer.group_discard(
+    #         self.room_group_name,
+    #         self.channel_name
+    #     )
+    # # async def send_pusher(self,senderID,message='hellow'):
+        
+    # #     'note this is a one on one chat so we get the ids all i need to do is to send the notifciation to the reciver'
+    #     # firstuserid,seconduserid = self.room_name.splite('and') 
+    #     # firstuserid = int(firstuserid)
+    #     # seconduserid = int(seconduserid)
+
+    #     # reciver_id = -1
+
+    #     # if seconduserid != int(senderID):
+    #     #     'this person is a reciver'
+    #     #     reciver_id = seconduserid
+    #     # if firstuserid != int(senderID):
+    #     #     'this person is a reciver'
+    #     #     reciver_id = firstuserid
+    #     # alert ={
+    #     #         'title':'membership app',
+    #     #         'body': message,
+    #     #     }
+    #     # response= beams_client.publish_to_users(
+    #     # user_ids=[f'{reciver_id}'],
+    #     # publish_body={
+    #     # 'apns': {
+    #     #     'aps': {
+    #     #     'alert': alert,
+    #     #     },
+    #     # },
+    #     # 'fcm': {
+    #     #     'notification': alert,
+    #     # },
+    #     # 'web': {
+    #     #     'notification':alert,
+    #     # },
+    #     # },
+    #     # )
+    # async def receive(self, text_data):
+    #     text_data_json = json.loads(text_data)
+    #     message = text_data_json['message']
+    #     send_user_id = text_data_json['send_user_id']
+    #     # tenant_name
+    #     # print({'schema anme':connection.schema_name})
+    #     print({'schema anme':self.tenant })
+    #     "here we setting the schema using the short name tha was sent from the front end"
+        
+    #     # firstuserid,seconduserid = self.room_name.splite('and') 
+    #     # await self.send_pusher(send_user_id)
+    #     await self.validate_user(send_user_id)
+    #     await self.create_chat(send_user_id,message)
+
+    #     await self.channel_layer.group_send(
+    #         self.room_group_name,
+    #         {
+    #             'type': 'chatroom_message',
+    #             'message': message,
+    #             'send_user_id': send_user_id,
+    #         }
+    #     )
+
+    # @sync_to_async
+    # def validate_user(self,send_user_id):
+    #     connection.set_schema(schema_name=self.tenant)
+    #     "check if user id exist in the data base"
+    #     print({'send_user_id':send_user_id})
+    #     if not get_user_model().objects.filter(id=send_user_id).exists():
+    #         print('does not exist')
+    #         raise CustomError(
+    #         message='User does not exist',
+    #         status_code=status.HTTP_400_BAD_REQUEST
+    #     )
+        
+    
+    # @sync_to_async
+    # def fill_user_name(self,send_user_id:int):
+    #     user =get_user_model().objects.get(id=send_user_id)
+    #     user_full_name = None
+    #     if user.user_type== 'members':
+    #         user_full_name = user.memeber.full_name
+    #     else:
+    #         user_full_name= f'admin: {user.first_name}'
+    #     return user_full_name
+    # @sync_to_async
+    # def create_chat(self,send_user_id,message):
+    #     "we get or create a group name"
+    #     connection.set_schema(schema_name=self.tenant)
+    #     chat_room ,created=  models.ChatRoom.objects.get_or_create(
+    #         room_name=self.room_group_name
+    #     )
+    #     chat_room.save()
+    #     "save the message to that group and u can add it wil the user id"
+    #     user = get_user_model().objects.get(id=send_user_id)
+    #     chat = models.Chat.objects.create(
+    #         chat_room=chat_room,
+    #         message=message,
+    #         user=user
+    #     )
+    #     chat.save()
+
+    # async def chatroom_message(self, event):
+
+    #     message = event['message']
+    #     send_user_id = event['send_user_id']
+    #     full_name = await self.fill_user_name(send_user_id) 
+    #     await self.send(text_data=json.dumps({
+    #         'message': message,
+    #         'send_user_id': send_user_id,
+    #         'full_name':full_name
+
+    #     }))
+
+    # @sync_to_async
+    # def validate_room(self):
+    #     """
+    #     Validate that the user has access to the specified chat room.
+    #     """
+    #     room_name = self.room_name
+    #     user = self.scope['user']
+
+    #     # Set the tenant schema
+    #     connection.set_schema(schema_name=self.tenant)
+
+    #     if room_name is None:
+    #         logger.error('Chat Room name is required')
+    #         raise CustomError(
+    #             message='Chat Room name is required',
+    #             status_code=status.HTTP_400_BAD_REQUEST
+    #         )
+
+    #     # Check if the chat room exists and if the user is part of it
+    #     chat_room_exists = models.Chat.objects.filter(
+    #         chat_room__room_name=room_name, 
+    #         user=user
+    #     ).exists()
+
+    #     if not chat_room_exists:
+    #         logger.error('Access Denied: User does not have access to the chat room')
+    #         raise CustomError(
+    #             message='Access Denied: You do not have access to the chat room',
+    #             status_code=status.HTTP_403_FORBIDDEN
+    #         )
+
+    # pass
 
 
 class CommiteeChatRoomConsumer(AsyncWebsocketConsumer):
